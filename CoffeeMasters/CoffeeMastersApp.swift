3467b6c54283e08b79e9519c4ba6f66165d3a717
HERYORDEJY <heryordejy.dev@gmail.com>
2023-07-16 15:40:10 +0100

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by  OYEBODE YUSUF AYODEJI on 7/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
